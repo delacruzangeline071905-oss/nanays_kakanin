ed7f053eb79407752d0f2941644d163f9c67a808
Angeline Arrogante <delacruzangeline071905@gmail.com>
2025-11-14 15:43:28 +0800

    ok kaayo

--- nanays_kakanin/nanays_kakanin/settings.py
+++ nanays_kakanin/nanays_kakanin/settings.py
@@ -100,3 +100,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Login URL configuration
 LOGIN_URL = '/login/'  # Redirect to /login/ instead of /accounts/login/
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
